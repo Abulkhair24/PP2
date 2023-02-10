d32890c7ea0605c6e495e4561fc536b8c31233e3
Abulkhair24 <abulkkhair1@gmail.com>
2023-02-10 21:50:09 +0600

    11

--- 3Lab/Function/12ex.py
+++ 3Lab/Function/12ex.py
@@ -1,15 +1,10 @@
-N=int(input())
-numbers = [0]*N
-for i in range(N):
-    numbers[i]=int(input())
-def histogram(nums):
-    k=len(nums)
-    d=k
-    g=0
-    while k>0:
-        while numbers[g]>0:
-            print("*")
-            numbers[g]=-1
-        g=+1
-    k=-1
-histogram(numbers)
+def histogram( items ):
+    for n in items:
+        output = ''
+        times = n
+        while( times > 0 ):
+          output += '*'
+          times = times - 1
+        print(output)
+
+histogram([2, 3, 6, 5])
